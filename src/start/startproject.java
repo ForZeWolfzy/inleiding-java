4f727560a8ab1faa0ca1b7b9d98d550dd7ab1048
ForZeWolfzy <gbolwerk@roc-dev.com>
2019-09-23 08:53:59 +0200

    8.1

---
+++ src/start/startproject.java
@@ -0,0 +1,17 @@
+import java.awt.*;
+import java.applet.*;
+import java.awt.event.*;
+
+public class startproject extends Applet {
+
+
+    public void init() {
+
+
+    }
+
+    public void paint(Graphics g) {
+
+    }
+
+}
