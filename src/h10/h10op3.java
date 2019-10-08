a627c8bfe8eebab11f569189b6d03b2f232aba97
ForZeWolfzy <gbolwerk@roc-dev.com>
2019-10-08 08:44:25 +0200

    h10

---
+++ src/h10/h10op3.java
@@ -0,0 +1,44 @@
+package h10;
+
+import java.applet.Applet;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class h10op3 extends Applet {
+    TextField tekstvak;
+    int maand;
+    Label maandlabel;
+    String[] maandNaam = {"januari", "februari", "maart", "april","mei","juni","juli","augustus","september","oktober","november","december"};
+    String[] maandDagen = {"31", "28", "31", "30","31,","30,","31","31","30","31","30","31"};
+    int inputValue = 0;
+
+    public void init() {
+        tekstvak = new TextField(20);
+        tekstvak.addActionListener(new MaandListener());
+        add(tekstvak);
+        maandlabel = new Label();
+        add(maandlabel);
+
+
+    }
+
+    public void paint(Graphics g){
+        g.drawString("hier komt de maand " + maandNaam[inputValue - 1] + " /dagen " + maandDagen[inputValue - 1], 50 ,50);
+    }
+
+    class MaandListener implements ActionListener {
+
+
+        public void actionPerformed(ActionEvent e) {
+
+            inputValue = Integer.parseInt(tekstvak.getText());
+
+            if (inputValue >= 1 && inputValue <= 12) {
+                repaint();
+            }
+
+
+        }
+    }
+}
